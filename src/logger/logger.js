75e8d0e22f78fdd251431eb11eb568058c784704
kashi <kashisharmaujjn@gmail.com>
2023-04-21 11:09:32 +0530

    GET APT Problem Update

--- src/logger/logger.js
+++ src/logger/logger.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
+let name1="Kashi"
 
 router.get('/test', function (req, res) {
-    res.send('Welcome to my application. I am Kashi and a part of FunctionUp Californium cohort.!')
+    res.send("Welcome to my application. I am "+name1 +" and a part of FunctionUp Californium cohort.!")
 });
 
 module.exports = router;
